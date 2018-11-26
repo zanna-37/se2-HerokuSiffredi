709007f37df81f0598342ea73da6123f4ec192bf
zanna-37 <43404980+zanna-37@users.noreply.github.com>
2018-11-26 23:31:58 +0100

    Heroku Procfile and HOST fix

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
 
 const db = require('./db');
 app.on('close', () => db.close()); //TODO <-- not sure
 
-app.listen(PORT, HOST, () => console.log(`App is started. Listening at ${HOST}, on port ${PORT}`));
+app.listen(PORT, () => console.log(`App is started. Listening on port ${PORT}`));
