bf5248a9d6efe470776aa698ff7c7b86386aea8b
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2018-11-27 20:06:10 +0100

    Added test cases for POST /v1/exams

--- routes/v1/exams.test.js
+++ routes/v1/exams.test.js
@@ -16,3 +16,60 @@ test(`GET ${route}`, () => {
             });
         });
 });
+
+// Three required inputs: examTemplateID:integer, ownerIDs:[integer], defaultDeadlineEnd:date
+// Inputs are passed as json in body
+describe(`POST ${route}`, () => {
+    const expectPostError = obj => {
+        const body = JSON.stringify(obj);
+        return request(app)
+            .post(route)
+            .send(body)
+            .then(res => {
+                expect(res.status).toBe(400);
+                expect(res.body).toHaveProperty('code');
+                expect(res.body).toHaveProperty('message');
+            });
+    };
+    const defaultBody = {
+        examTemplateID: 23,
+        ownerIDs: [5, 10, 70],
+        defaultDeadlineEnd: '20019-10-19 10:23:54+02'
+    };
+
+    test('no body', () =>
+        request(app)
+            .post(route)
+            .then(res => {
+                expect(res.status).toBe(400);
+                expect(res.body).toHaveProperty('code');
+                expect(res.body).toHaveProperty('message');
+            })
+    );
+    test('pass empty json in body', () => expectPostError({}));
+    test('pass wrong properties in json', () => expectPostError({
+        aaa: 'bbb',
+        ccc: 'ddd',
+        eee: 'fff',
+        ggg: 'hhh'
+    }));
+    test('examTemplateID is null', () => expectPostError({ ...defaultBody, examTemplateID: null }));
+    test('ownerIDs is null', () => expectPostError({ ...defaultBody, ownerIDs: null }));
+    test('defaultDeadlineEnd is null', () => expectPostError({ ...defaultBody, defaultDeadlineEnd: null }));
+    test('examTemplateID is undefined', () => expectPostError({ ...defaultBody, examTemplateID: undefined }));
+    test('ownerIDs is undefined', () => expectPostError({ ...defaultBody, ownerIDs: undefined }));
+    test('defaultDeadlineEnd is undefined', () => expectPostError({ ...defaultBody, defaultDeadlineEnd: undefined }));
+    test('examTemplateID string instead of integer', () => expectPostError({ ...defaultBody, examTemplateID: defaultBody.examTemplateID.toString() }));
+    test('ownerIDs string instead of integer', () => expectPostError({ ...defaultBody, ownerIDs: defaultBody.ownerIDs.map(toString) }));
+    test('examTemplateID integer instead of string', () => expectPostError({ ...defaultBody, defaultDeadlineEnd: parseInt(defaultBody.defaultDeadlineEnd) }));
+
+    test('using arbitrary body', () =>
+        request(app)
+            .post(route)
+            .send(defaultBody)
+            .then(res => {
+                expect(res.status).toBe(201);
+                expect(res.body).toHaveProperty('id');
+            })
+    );
+});
