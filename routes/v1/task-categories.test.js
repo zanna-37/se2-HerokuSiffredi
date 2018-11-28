ea4c1efb58bfe3a864d665d53dc7bd47c30f9259
zanna-37 <43404980+zanna-37@users.noreply.github.com>
2018-11-28 02:33:07 +0100

    Added tests for POST method on /task-categories

--- routes/v1/task-categories.test.js
+++ routes/v1/task-categories.test.js
@@ -1,22 +1,51 @@
-let db;
+const db = require('../../db');
+require('supertest');
+const request = require('supertest');
+const app = require('../../app');
 
 beforeAll(() => {
-    db = require('../../db');
 });
 
 afterAll(() => {
-    // noinspection JSIgnoredPromiseFromCall
-    db.close();
+    return db.close();
 });
 
-test('v1_get response to have id and name', () => {
-    const request = require('supertest');
-    const app = require('../../app');
-    return request(app).get('/v1/task-categories').then(response => {
-        expect(response.statusCode).toBe(200);
-        response.body.forEach(function (task_category) {
-            expect(task_category).toHaveProperty('id');
-            expect(task_category).toHaveProperty('name');
+describe('v1_get', () => {
+    test('response to have id and name', async () => {
+        await request(app).get('/v1/task-categories').then(response => {
+            expect(response.statusCode).toBe(200);
+            response.body.forEach(function (task_category) {
+                expect(task_category).toHaveProperty('id');
+                expect(task_category).toHaveProperty('name');
+            });
+        });
+    });
+});
+
+describe('v1_post', () => {
+    test('empty body', async () => {
+        expect.assertions(3);
+        await request(app).post('/v1/task-categories').then(response => {
+            expect(response.statusCode).toBe(400);
+            expect(response.body).toHaveProperty('code');
+            expect(response.body).toHaveProperty('message');
+        });
+    });
+
+    test('wrong parameters', async () => {
+        expect.assertions(3);
+        await request(app).post('/v1/task-categories').send({foo: 'bla bla bla'}).then(response => {
+            expect(response.statusCode).toBe(400);
+            expect(response.body).toHaveProperty('code');
+            expect(response.body).toHaveProperty('message');
+        });
+    });
+
+    test('right parameter', async () => {
+        expect.assertions(2);
+        await request(app).post('/v1/task-categories').send({name: 'testCategory'}).then(response => {
+            expect(response.statusCode).toBe(201);
+            expect(response.body).toHaveProperty('id');
         });
     });
 });
