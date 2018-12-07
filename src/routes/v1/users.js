5ace67e11696201a053cef97f9947474804c7977
Miriam <mpunzi97@gmail.com>
2018-12-07 16:34:50 +0100

    refs #31 #34
    - added GET on /users/{id}
    - added PUT on /users/{id}

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -35,4 +35,48 @@ router.post('/', function (req, res) {
     }
 });
 
+router.get('/:id', function (req, res) {
+    const id = req.params.id;
+
+    model_users
+        .findOne({where: {id: id}})
+        .then(numRowsRetrived => {
+            if (numRowsRetrived == null)
+                res.status(404).send({code: 404, message: 'Not Found: id not found'});
+            else
+                res.status(200).send(numRowsRetrived.dataValues);
+        });
+});
+
+router.put('/:id', function (req, res) {
+    const req_body = req.body;
+    const id = req.params.id;
+    const keys = Object.keys(req_body);
+
+    if (!(req_body.hasOwnProperty('name') && req_body.hasOwnProperty('surname') && req_body.hasOwnProperty('student_number')))
+        res.status(400).send({code: 400, message: 'Bad Request: missing parameters'});
+    else if (req_body.name == null || req_body.surname == null || req_body.student_number == null)
+        res.status(400).send({code: 400, message: 'Bad Request: missing values of parameters'});
+    else if (!(typeof req_body.name === 'string' && typeof req_body.surname === 'string' && Number.isInteger(req_body.student_number)))
+        res.status(400).send({code: 400, message: 'Bad Request: invalid values'});
+    else if (req_body.hasOwnProperty('average'))
+        res.status(400).send({code: 400, message: 'Bad Request: cannot set the average'});
+    else if (keys.some(key => {
+        return (key !== 'name' && key !== 'surname' && key !== 'student_number');
+    }))
+        res.status(400).send({code: 400, message: 'Bad Request: invalid parameters'});
+    else if (req_body.student_number <= 0)
+        res.status(400).send({code: 400, message: 'Bad Request: student number has to be > 0'});
+    else {
+        model_users
+            .update({...req_body}, {where: {id: id}})
+            .then(numRowsModified => {
+                if (Number.parseInt(numRowsModified) === 0)
+                    res.status(404).send({code: 404, message: 'Not Found: id not found'});
+                else
+                    res.status(204).send();
+            });
+    }
+});
+
 module.exports = router;
