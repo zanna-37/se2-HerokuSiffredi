7dac7b654c57ad76e6ec319a1868533b30c60f65
Miriam <mpunzi97@gmail.com>
2018-12-07 16:33:37 +0100

    refs #31 #34
    - added tests of GET on /users/{id}
    - added tests of PUT on /users/{id}

--- test/routes/v1/users.test.js
+++ test/routes/v1/users.test.js
@@ -6,6 +6,12 @@ afterAll(() => db.close());
 
 const route = '/v1/users';
 
+const rightBody = {
+    name: 'Miriam',
+    surname: 'Punzi',
+    student_number: 186574
+};
+
 describe('GET /v1/users', () => {
     test('Test if it retrieves all the right attributes', () => {
         return request(app)
@@ -18,7 +24,19 @@ describe('GET /v1/users', () => {
                     expect(user).toHaveProperty('name');
                     expect(user).toHaveProperty('surname');
                     expect(user).toHaveProperty('student_number');
-                    expect(user).toHaveProperty('average');
+                });
+            });
+    });
+
+    test('Test if it retrieves the right type of attributes', () => {
+        return request(app)
+            .get(route)
+            .then(response => {
+                response.body.forEach(function (user) {
+                    expect(typeof user.id).toBe('number');
+                    expect(typeof user.name).toBe('string');
+                    expect(typeof user.surname).toBe('string');
+                    expect(typeof user.student_number).toBe('number');
                 });
             });
     });
@@ -47,13 +65,6 @@ describe('POST /v1/users', () => {
             });
     };
 
-    // json for a successful post
-    const rightBody = {
-        name: 'Miriam',
-        surname: 'Punzi',
-        student_number: 186574
-    };
-
     // Tests right body
     test('Test right body', () => exceptPostOK(rightBody));
 
@@ -77,4 +88,120 @@ describe('POST /v1/users', () => {
     test('Test student number is not number', () => expectPostError({...rightBody, student_number: 'hello'}));
     test('Test student number < 0', () => expectPostError({...rightBody, student_number: -12}));
     test('Test student number = 0', () => expectPostError({...rightBody, student_number: 0}));
+});
+
+describe('GET /v1/users/{id}', () => {
+    let idTempUser;
+
+    request(app)
+        .post(route)
+        .send({...rightBody})
+        .then(result => {
+            idTempUser = result.body.id;
+            return idTempUser;
+        });
+
+    test('Test if it retrieves the right attributes', () => {
+        return request(app)
+            .get(route + '/' + idTempUser)
+            .then(response => {
+                expect(response.statusCode).toBe(200);
+                expect(response.body).toBeInstanceOf(Object);
+                expect(response.body).toHaveProperty('id');
+                expect(response.body).toHaveProperty('name');
+                expect(response.body).toHaveProperty('surname');
+                expect(response.body).toHaveProperty('student_number');
+            });
+    });
+
+    test('Test if it retrieves the right type of attributes', () => {
+        return request(app)
+            .get(route + '/' + idTempUser)
+            .then(response => {
+                expect(typeof response.body.id).toBe('number');
+                expect(typeof response.body.name).toBe('string');
+                expect(typeof response.body.surname).toBe('string');
+                expect(typeof response.body.student_number).toBe('number');
+            });
+    });
+
+    test('Test an invalid id', () => {
+        return request(app)
+            .get(route + '/' + -1)
+            .then(response => {
+                expect(response.statusCode).toBe(404);
+                expect(response.body).toHaveProperty('code');
+                expect(response.body).toHaveProperty('message');
+            });
+    });
+});
+
+describe('PUT /v1/users/{id}', () => {
+    let idTempUser;
+
+    request(app)
+        .post(route)
+        .send({...rightBody})
+        .then(result => {
+            idTempUser = result.body.id;
+            return idTempUser;
+        });
+
+    const expectPutBadRequest = body => {
+        return request(app)
+            .put(route + '/' + idTempUser)
+            .send(body)
+            .then(res => {
+                expect(res.status).toBe(400);
+                expect(res.body).toHaveProperty('code');
+                expect(res.body).toHaveProperty('message');
+            });
+    };
+
+    const expectPutNotFound = body => {
+        return request(app)
+            .put(route + '/-1')
+            .send(body)
+            .then(res => {
+                expect(res.status).toBe(404);
+                expect(res.body).toHaveProperty('code');
+                expect(res.body).toHaveProperty('message');
+            });
+    };
+
+    const exceptPutOK = body => {
+        return request(app)
+            .put(route + '/' + idTempUser)
+            .send(body)
+            .then(res => {
+                expect(res.status).toBe(204);
+            });
+    };
+
+    // Tests right body
+    test('Test right body', () => exceptPutOK(rightBody));
+
+    // Test body missing parameters
+    test('Test name undefined', () => expectPutBadRequest({...rightBody, name: undefined}));
+    test('Test surname undefined', () => expectPutBadRequest({...rightBody, surname: undefined}));
+    test('Test student number undefined', () => expectPutBadRequest({...rightBody, student_number: undefined}));
+
+    // Test body with invalid parameters
+    test('Test body with average in json', () => expectPutBadRequest({...rightBody, average: 18}));
+    test('Test body with strange parameters', () => expectPutBadRequest({...rightBody, birth: '08/04/1997'}));
+
+    // Test body missing values of parameters
+    test('Test name null', () => expectPutBadRequest({...rightBody, name: null}));
+    test('Test surname null', () => expectPutBadRequest({...rightBody, surname: null}));
+    test('Test student number null', () => expectPutBadRequest({...rightBody, student_number: null}));
+
+    // Test body with invalid values
+    test('Test name is not string', () => expectPutBadRequest({...rightBody, name: 1}));
+    test('Test surname is not string', () => expectPutBadRequest({...rightBody, surname: 1}));
+    test('Test student number is not number', () => expectPutBadRequest({...rightBody, student_number: 'hello'}));
+    test('Test student number < 0', () => expectPutBadRequest({...rightBody, student_number: -12}));
+    test('Test student number = 0', () => expectPutBadRequest({...rightBody, student_number: 0}));
+
+    // Test an id not in db
+    test('Test id not in db', () => expectPutNotFound({...rightBody}));
 });
