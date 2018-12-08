202a814f62c21b76558fb477f874ddb0c60f705a
Federico Favotto <federico.favotto@studenti.unitn.it>
2018-12-08 12:28:39 +0100

    resolved #21
    - fixed GET tests

--- test/routes/v1/tasks.test.js
+++ test/routes/v1/tasks.test.js
@@ -1,27 +1,28 @@
 const db = require('../../../src/db');
+const request = require('supertest');
+const app = require('../../../src/app');
 
 afterAll(() => db.close());
 
-test('tasks_v1 get response must have:\n' +
-    '\t- id\n' +
-    '\t- exerciseText\n' +
-    '\t- answers[]\n' +
-    '\t- explanation\n' +
-    '\t- categoryId\n' +
-    '\t- totalPoints\n' +
-    '\t- lastEdit', () => {
-    const request = require('supertest');
-    const app = require('../../../src/app');
-    return request(app).get('/v1/tasks').then(response => {
-        expect(response.statusCode).toBe(200);
-        response.body.forEach(function (task) {
-            expect(task).toHaveProperty('id');
-            expect(task).toHaveProperty('exerciseText');
-            expect(task).toHaveProperty('answers');
-            expect(task).toHaveProperty('explanation');
-            expect(task).toHaveProperty('categoryId');
-            expect(task).toHaveProperty('totalPoints');
-            expect(task).toHaveProperty('lastEdit');
-        });
+const route = '/v1/tasks';
+
+describe(`GET ${route}`, () => {
+    test('check attributes', () => {
+        return request(app)
+            .get(route)
+            .then(response => {
+                expect(response.statusCode).toBe(200);
+                response.body.forEach((task) => {
+                    expect(task).toEqual(
+                        {
+                            id: expect.any(Number),
+                            exerciseText: expect.any(String),
+                            rightAnswer: expect.any(String),
+                            totalPoints: expect.any(Number),
+                            average: expect.any(Number),
+                            categoryIDs: expect.any(Array(expect.any(Number)))
+                        });
+                });
+            });
     });
 });
