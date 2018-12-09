c1f322a2bdcc859e701c6402e51279c814dbbd63
Federico Favotto <federico.favotto@studenti.unitn.it>
2018-12-09 12:10:28 +0100

    fixed GET tests

--- test/routes/v1/tasks.test.js
+++ test/routes/v1/tasks.test.js
@@ -1,28 +1,43 @@
 const db = require('../../../src/db');
 const request = require('supertest');
 const app = require('../../../src/app');
+const model_tasks = require('../../../src/models/v1/tasks');
 
 afterAll(() => db.close());
 
 const route = '/v1/tasks';
 
+let demoTask = {
+    'exerciseText': 'demoText',
+    'rightAnswer': 'demoAnswer',
+    'totalPoints': 1,
+    'average': 1,
+    'categoryIDs': [1,2,3]
+};
+
 describe(`GET ${route}`, () => {
-    test('check attributes', () => {
-        return request(app)
+    test('right attributes', async () => {
+        const tmp1 = await model_tasks.create(demoTask);
+        const tmp2 = await model_tasks.create(demoTask);
+
+        await request(app)
             .get(route)
             .then(response => {
                 expect(response.statusCode).toBe(200);
-                response.body.forEach((task) => {
-                    expect(task).toEqual(
-                        {
-                            id: expect.any(Number),
-                            exerciseText: expect.any(String),
-                            rightAnswer: expect.any(String),
-                            totalPoints: expect.any(Number),
-                            average: expect.any(Number),
-                            categoryIDs: expect.any(Array(expect.any(Number)))
-                        });
+                expect(response.body).toBeInstanceOf(Array);
+                response.body.forEach( task => {
+                    expect(task).toEqual({
+                        'id': expect.any(Number),
+                        'exerciseText': expect.any(String),
+                        'rightAnswer': expect.any(String),
+                        'totalPoints': expect.any(Number),
+                        'average': expect.any(Number),
+                        'categoryIDs': expect.arrayContaining([expect.any(Number)])
+                    });
                 });
             });
+
+        tmp1.destroy();
+        tmp2.destroy();
     });
 });
