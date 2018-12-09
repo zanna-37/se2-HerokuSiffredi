b4947156d7432581cbca11106e2b8da6846ece9a
Federico Favotto <federico.favotto@studenti.unitn.it>
2018-12-09 17:00:30 +0100

    added tests for POST method

--- test/routes/v1/tasks.test.js
+++ test/routes/v1/tasks.test.js
@@ -7,12 +7,12 @@ afterAll(() => db.close());
 
 const route = '/v1/tasks';
 
-let demoTask = {
-    'exerciseText': 'demoText',
-    'rightAnswer': 'demoAnswer',
-    'totalPoints': 1,
-    'average': 1,
-    'categoryIDs': [1,2,3]
+const demoTask = {
+    exerciseText: 'demoText',
+    rightAnswer: 'demoAnswer',
+    totalPoints: 1,
+    average: 1,
+    categoryIDs: [1, 2, 3]
 };
 
 describe(`GET ${route}`, () => {
@@ -25,7 +25,7 @@ describe(`GET ${route}`, () => {
             .then(response => {
                 expect(response.statusCode).toBe(200);
                 expect(response.body).toBeInstanceOf(Array);
-                response.body.forEach( task => {
+                response.body.forEach(task => {
                     expect(task).toEqual({
                         id: expect.any(Number),
                         exerciseText: expect.any(String),
@@ -119,6 +119,86 @@ describe(`GET ${route}/:id`, () => {
             .get(route + '/' + nowhereID)
             .then(res => {
                 expect(res.statusCode).toBe(404);
+                expect(res.body).toEqual(
+                    {
+                        code: expect.any(Number),
+                        message: expect.any(String)
+                    }
+                );
+            });
+    });
+});
+
+describe(`POST ${route}`, () => {
+    const newTask = {...demoTask, average: undefined};
+
+    const postOk = body => {
+        expect.assertions(2);
+        return request(app)
+            .post(route)
+            .send(body)
+            .then(res => {
+                expect(res.statusCode).toBe(201);
+                expect(res.body).toEqual({id: expect.any(Number)});
+                return res.body.id;
+            });
+    };
+
+    const postError = body => {
+        expect.assertions(2);
+        return request(app)
+            .post(route)
+            .send(body)
+            .then(res => {
+                expect(res.status).toBe(400);
+                expect(res.body).toEqual(
+                    {
+                        code: expect.any(Number),
+                        message: expect.any(String)
+                    }
+                );
+            });
+    };
+
+    test('valid parameters', async () => {
+        const tmp = await postOk(newTask);
+        model_tasks.destroy({
+            where: {
+                id: tmp
+            }
+        });
+
+    });
+
+    test('exerciseText is undefined', async () => postError({...newTask, exerciseText: undefined}));
+    test('rightAnswer is undefined', async () => postError({...newTask, rightAnswer: undefined}));
+    test('totalPoints is undefined', async () => postError({...newTask, totalPoints: undefined}));
+    test('categoryIDs is undefined', async () => postError({...newTask, categoryIDs: undefined}));
+
+    test('exerciseText is null', async () => postError({...newTask, exerciseText: null}));
+    test('rightAnswer is null', async () => postError({...newTask, rightAnswer: null}));
+    test('totalPoints is null', async () => postError({...newTask, totalPoints: null}));
+    test('categoryIDs is null', async () => postError({...newTask, categoryIDs: null}));
+
+    test('setting average on creation', async () => postError({...newTask, average: 10}));
+    test('exceeding parameters', async () => postError({...newTask, oneMore: true}));
+
+    test('exerciseText value is not valid', async () => postError({...newTask, exerciseText: 10}));
+    test('rightAnswer value is not valid', async () => postError({...newTask, rightAnswer: 10}));
+    test('totalPoints value is not valid', async () => postError({...newTask, totalPoints: 'wrong'}));
+    test('categoryIDs is not an array', async () => postError({...newTask, categoryIDs: 10}));
+    test('zero-length categoryIDs', async () => postError({...newTask, categoryIDs: []}));
+    test('categoryIDs has some values which are not numbers', async () => postError({...newTask, categoryIDs: [1,'wrong',3]}));
+});
+
+describe(`POST ${route}/:id`, () => {
+    test('Method not allowed', async () => {
+        const randID = Math.round(Math.random() * 100);
+        expect.assertions(2);
+        await request(app)
+            .post(route + '/' + randID)
+            .then(res => {
+                expect(res.status).toBe(405);
                 expect(res.body).toEqual(
                     {
                         code: expect.any(Number),
