1709dd4a168d1374780e594fe57edb71769a3918
Federico Favotto <federico.favotto@studenti.unitn.it>
2018-12-09 12:30:08 +0100

    resolved #26 GET /tasks/:id
    - implementation and tests for single-item GET
    - some fixes

--- src/routes/v1/tasks.js
+++ src/routes/v1/tasks.js
@@ -3,10 +3,39 @@ const router = express.Router();
 
 const model_tasks = require('../../models/v1/tasks');
 
-router.get('/', function (req, res) {
-    model_tasks.findAll().then(tasks => {
-        res.send(tasks);
-    });
+router.get('/', (req, res) => {
+    res.set('Accept', 'application/json');
+    model_tasks
+        .findAll()
+        .then(tasks => res.send(tasks));
+});
+
+router.get('/:id', (req, res) => {
+    const id = Number.parseInt(req.params.id);
+    res.set('Accept', 'application/json');
+    if (isNaN(id)) {
+        res
+            .status(400)
+            .send({
+                code: 400,
+                message: 'The specified ID is not valid'
+            });
+    } else {
+        model_tasks
+            .findByPk(id)
+            .then(tasks => {
+                if (tasks == null) {
+                    res
+                        .status(404)
+                        .send({
+                            code: 404,
+                            message: 'Task not found'
+                        });
+                } else {
+                    res.send(tasks);
+                }
+            });
+    }
 });
 
 module.exports = router;
--- test/routes/v1/tasks.test.js
+++ test/routes/v1/tasks.test.js
@@ -27,17 +27,104 @@ describe(`GET ${route}`, () => {
                 expect(response.body).toBeInstanceOf(Array);
                 response.body.forEach( task => {
                     expect(task).toEqual({
-                        'id': expect.any(Number),
-                        'exerciseText': expect.any(String),
-                        'rightAnswer': expect.any(String),
-                        'totalPoints': expect.any(Number),
-                        'average': expect.any(Number),
-                        'categoryIDs': expect.arrayContaining([expect.any(Number)])
+                        id: expect.any(Number),
+                        exerciseText: expect.any(String),
+                        rightAnswer: expect.any(String),
+                        totalPoints: expect.any(Number),
+                        average: expect.any(Number),
+                        categoryIDs: expect.arrayContaining([expect.any(Number)])
                     });
                 });
             });
 
         tmp1.destroy();
         tmp2.destroy();
+    });
+});
+
+describe(`GET ${route}/:id`, () => {
+    test('passed ID is valid', async () => {
+        const tmp = await model_tasks.create(demoTask);
+        expect.assertions(2);
+        await request(app)
+            .get(route + '/' + tmp.id)
+            .then(res => {
+                expect(res.statusCode).toBe(200);
+                expect(res.body).toEqual(
+                    {
+                        id: expect.any(Number),
+                        exerciseText: expect.any(String),
+                        rightAnswer: expect.any(String),
+                        totalPoints: expect.any(Number),
+                        average: expect.any(Number),
+                        categoryIDs: expect.arrayContaining([expect.any(Number)])
+                    }
+                );
+            });
+        tmp.destroy();
+    });
+
+    test('passed ID is not valid', async () => {
+        const wrongID = 'hi';
+        expect.assertions(2);
+        await request(app)
+            .get(route + '/' + wrongID)
+            .then(res => {
+                expect(res.statusCode).toBe(400);
+                expect(res.body).toEqual(
+                    {
+                        code: expect.any(Number),
+                        message: expect.any(String)
+                    }
+                );
+            });
+    });
+
+    test('passed ID is empty', async () => {
+        let emptyID;
+        expect.assertions(2);
+        await request(app)
+            .get(route + '/' + emptyID)
+            .then(res => {
+                expect(res.statusCode).toBe(400);
+                expect(res.body).toEqual(
+                    {
+                        code: expect.any(Number),
+                        message: expect.any(String)
+                    }
+                );
+            });
+    });
+
+    test('passed ID is null', async () => {
+        const nullID = null;
+        expect.assertions(2);
+        await request(app)
+            .get(route + '/' + nullID)
+            .then(res => {
+                expect(res.statusCode).toBe(400);
+                expect(res.body).toEqual(
+                    {
+                        code: expect.any(Number),
+                        message: expect.any(String)
+                    }
+                );
+            });
+    });
+
+    test('passed ID not found', async () => {
+        const nowhereID = 0;
+        expect.assertions(2);
+        await request(app)
+            .get(route + '/' + nowhereID)
+            .then(res => {
+                expect(res.statusCode).toBe(404);
+                expect(res.body).toEqual(
+                    {
+                        code: expect.any(Number),
+                        message: expect.any(String)
+                    }
+                );
+            });
     });
 });
