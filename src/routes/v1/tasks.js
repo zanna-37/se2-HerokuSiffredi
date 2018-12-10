3c442e35e3f832896695f5c19386f3b8ecedf91d
Federico Favotto <federico.favotto@studenti.unitn.it>
2018-12-10 15:26:20 +0100

    PUT/:id method implementation
    - added PUT/:id implementation
    - removed average

--- src/routes/v1/tasks.js
+++ src/routes/v1/tasks.js
@@ -48,8 +48,6 @@ router.post('/', (req, res) => {
         params.hasOwnProperty('categoryIDs'))) {
 
         res.status(400).send({code: 400, message: 'Missing parameters'});
-    } else if (params.hasOwnProperty('average')) {
-        res.status(400).send({code: 400, message: 'Cannot set average on creation'});
     } else if (
         Object.keys(params).some(key => {
             return (key !== 'exerciseText' &&
@@ -89,6 +87,62 @@ router.post('/:id', (req, res) => {
             code: 405,
             message: 'Method not allowed'
         });
+});
+
+router.put('/:id', (req, res) => {
+    const params = req.body;
+    const id = Number.parseInt(req.params.id);
+    res.set('Accept', 'application/json');
+
+    if (!Number.isInteger(id)) {
+        res.status(400).send({code: 400, message: 'Wrong type url ID'});
+    } else if (!(params.hasOwnProperty('id') &&
+        params.hasOwnProperty('exerciseText') &&
+        params.hasOwnProperty('rightAnswer') &&
+        params.hasOwnProperty('totalPoints') &&
+        params.hasOwnProperty('categoryIDs'))) {
+
+        res.status(400).send({code: 400, message: 'Missing parameters'});
+    } else if (
+        Object.keys(params).some(key => {
+            return (key !== 'id' &&
+                key !== 'exerciseText' &&
+                key !== 'rightAnswer' &&
+                key !== 'totalPoints' &&
+                key !== 'categoryIDs');
+        })) {
+
+        res.status(400).send({code: 400, message: 'Exceeding parameters'});
+    } else if (params.id == null ||
+        params.exerciseText == null ||
+        params.rightAnswer == null ||
+        params.totalPoints == null ||
+        params.categoryIDs == null) {
+
+        res.status(400).send({code: 400, message: 'Some required parameters are null'});
+    } else if (!(Number.isInteger(params.id) &&
+        typeof params.exerciseText === 'string' &&
+        typeof params.rightAnswer === 'string' &&
+        !isNaN(params.totalPoints) &&
+        Array.isArray(params.categoryIDs) &&
+        params.categoryIDs.length !== 0 &&
+        params.categoryIDs.every(item => Number.isInteger(item)))) {
+
+        res.status(400).send({code: 400, message: 'Wrong type parameters'});
+    } else if (id !== params.id) {
+        res.status(400).send({code: 400, message: 'The url ID and the one in the request body differ'});
+    } else {
+        model_tasks
+            .update(params, {where: {id: id}})
+            .then(rowsUpdatedArray => {
+                const rowsUpdated = rowsUpdatedArray[0];
+                if (rowsUpdated !== 1) {
+                    res.status(404).send({code: 404, message: 'Task not found'});
+                } else {
+                    res.status(204).send();
+                }
+            });
+    }
 });
 
 module.exports = router;
